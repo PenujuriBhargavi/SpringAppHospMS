064e18343f1c70cc09fc027769b461de1ba4c263
PenujuriBhargavi <83275889+PenujuriBhargavi@users.noreply.github.com>
2021-05-26 14:49:38 +0530

    Delete HospitalManagementSystem directory

--- HospitalManagementSystem/src/main/java/com/hms/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.hms.dao;
-
-import com.hms.entity.DoctorRegister;
-
-
-public interface AdminDao {
-	//public void doctorRegistration(DoctorRegister doctorregister);
-
-}
--- HospitalManagementSystem/src/main/java/com/hms/logging/LoggingAspect.java
+++
@@ -1,5 +0,0 @@
-package com.hms.logging;
-
-public class LoggingAspect {
-
-}
